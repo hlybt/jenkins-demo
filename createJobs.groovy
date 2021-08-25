4aa9626347ee1fccda030e455673a0cf44fa296c
hlybt <hulyabt@gmail.com>
2021-08-25 19:34:05 +0200

    Create a pipeline job for 'favourite-quotes' project.

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,22 @@
 pipelineJob('pipelineJob') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
-        }
-    }
+	definition {
+		cps {
+			script(readFileFromWorkspace('pipelineJob.groovy'))
+			sandbox()
+		}
+	}
+}
+pipelineJob('favourite-quotes') {
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url 'https://github.com/hlybt/favourite-quotes.git'
+					}
+					branch 'main'
+				}
+			}
+		}
+	}
 }
